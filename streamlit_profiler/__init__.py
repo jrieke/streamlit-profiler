973d789910ce1549cb3b24e2416a9612e683bd51
Johannes Rieke <johannes.rieke@gmail.com>
2021-12-19 01:06:32 +0100

    Make higher

--- streamlit_profiler/__init__.py
+++ streamlit_profiler/__init__.py
@@ -5,5 +5,5 @@ from pyinstrument import Profiler as OriginalProfiler
 class Profiler(OriginalProfiler):
     def __exit__(self, type, value, traceback):
         result = super().__exit__(type, value, traceback)
-        st.components.v1.html(self.output_html(), height=400, scrolling=True)
+        st.components.v1.html(self.output_html(), height=600, scrolling=True)
         return result
